0a7376054686bad86c04c615616da5db1b30edba
wangjingang <wangjingang@chexiang.com>
2015-08-05 10:12:52 +0800

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        uglify: {
+            core: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/main/resources',
+                    src: 'public/js/**/*.js',
+                    dest: "target/classes",
+                    ext: '.min.js'
+
+                }]
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
